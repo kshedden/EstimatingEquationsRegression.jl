c1f436def1101f53fd65fab6f30dc2147666b91c
Kerby Shedden <kshedden@umich.edu>
2023-02-12 15:34:55 -0500

    update stata example

--- examples/compare_stata.jl
+++ examples/compare_stata.jl
@@ -1,23 +1,38 @@
 #=
-A simple example using a dataset from the Stata
-documentation.  Use the link below to obtain
-the data.
+A simple example using a dataset from the Stata documentation.
+
+Use the link below to obtain the data.
 
 http://www.stata-press.com/data/r13/nlswork2.dta
+
+Compare to the results here:
+
+https://www.stata.com/manuals13/xtxtgee.pdf
 =#
 
-using StatFiles, GEE, GLM, DataFrames, StatsModels, Distributions, Statistics
+using StatFiles
+using GEE
+using GLM
+using DataFrames
+using StatsModels
+using Distributions
+using Statistics
 
 # Fit a model to the nlswork2  data
 d1 = DataFrame(load("nlswork2.dta"))
 
 d1 = d1[:, [:ln_wage, :grade, :age, :idcode]]
 d1 = d1[completecases(d1), :]
+d1 = disallowmissing(d1)
 
-d1[:, :age_c] = d1[:, :age] .- mean(d1[:, :age])
+d1[!, :ln_wage] = Float64.(d1[:, :ln_wage])
+d1[!, :grade] = Float64.(d1[:, :grade])
+d1[!, :age] = Float64.(d1[:, :age])
+d1[:, :age2] = d1[:, :age].^2
 
+# Fit a linear model with GEE using independent working correlation.
 m1 = gee(
-    @formula(ln_wage ~ grade + age + age_c^2),
+    @formula(ln_wage ~ grade + age + age2),
     d1,
     d1[:, :idcode],
     Normal(),
@@ -27,7 +42,7 @@ m1 = gee(
 disp1 = dispersion(m1.model)
 
 m2 = gee(
-    @formula(ln_wage ~ grade + age + age_c^2),
+    @formula(ln_wage ~ grade + age + age2),
     d1,
     d1[:, :idcode],
     Normal(),
